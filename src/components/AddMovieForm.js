db241fde8b0e5502f0ade599461f556e1d23be7c
AytacSahin <haytacsahin@gmail.com>
2023-05-24 04:42:05 +0300

    S10G2 Tamamlandı.

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { addMovie } from './../actions/movieActions';
-
+import { useDispatch } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 
-const AddMovieForm = (props) => {
+const AddMovieForm = () => {
   const { push } = useHistory();
-
+  const dispatch = useDispatch();
   const [movie, setMovie] = useState({
     title: "",
     director: "",
@@ -22,6 +22,9 @@ const AddMovieForm = (props) => {
   }
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addMovie({...movie, id: Date.now()}));
+    push("/movies")
   }
 
   const { title, director, genre, metascore, description } = movie;
